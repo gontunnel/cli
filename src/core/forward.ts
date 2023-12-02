1dd4c2c07e098f002af99d7b1f4c5cb9499ecf3b
Prince Codes <princecodes247@gmail.com>
2023-12-02 17:02:10 +0100

    optimise proxy server

--- src/core/forward.ts
+++ src/core/forward.ts
@@ -1,38 +1,35 @@
 import http, { IncomingMessage, ServerResponse } from "http";
-import https from "https";
-import { createProxyMiddleware } from "http-proxy-middleware";
+import { createProxyMiddleware, RequestHandler } from "http-proxy-middleware";
 import DomainStore from "../utils/store";
-import findDomain from "../utils/findDomain";
-import generateSelfSignedWildcardCertificate from "./ssl";
 
 const PORT1 = 80;
-const PORT2 = 443;
 
 let server: http.Server | null = null; // Store the server instance for later use
-let server2: http.Server | null = null; // Store the server instance for later use
 
-// Start the server
-function startProxyServer(verbose?: boolean) {
-  // Custom domains
-
-  // Create a proxy middleware to forward requests to the Node.js server
-  const proxy = createProxyMiddleware({
+// Extracted the proxy creation logic into a function for better readability
+function createProxy(): RequestHandler {
+  return createProxyMiddleware({
     target: "http://localhost:3000",
     changeOrigin: true, // Necessary for virtual hosted sites
     router: function (req: IncomingMessage) {
-      const domain = findDomain(req.headers.host as string);
-      // if (!domain) return undefined;
-      // let url = domain.value;
-      // // Check if the URL has a protocol, if not, attach "http://"
-      // if (url && !url.includes("://")) {
-      //   url = "http://" + url;
-      // }
+      const domain = DomainStore.getDomain(req.headers.host as string);
+      if (!domain) return undefined;
+      let url = domain.value;
+      // Check if the URL has a protocol, if not, attach "http://"
+      if (url && !url.includes("://")) {
+        url = "http://" + url;
+      }
 
-      // return url;
-      return "http://localhost:5000";
+      return url;
     },
     // logLevel: verbose ? "debug" : "silent",
   });
+}
+
+// Start the server
+function startProxyServer(verbose?: boolean) {
+  // Create a proxy middleware to forward requests to the Node.js server
+  const proxy = createProxy();
 
   // Create the server with a request handler
   const serverHandler = (req: any, res: any) => {
@@ -52,22 +49,6 @@ function startProxyServer(verbose?: boolean) {
   } else {
     if (verbose) console.log("Server is already running.");
   }
-
-  // if (!server2) {
-  //   // Generate self-signed wildcard SSL certificate for *.local
-  //   const { privateKey, certificate } =
-  //     generateSelfSignedWildcardCertificate("local");
-  //   const sslOptions = {
-  //     key: privateKey,
-  //     cert: certificate,
-  //   };
-  //   server2 = https.createServer(sslOptions, serverHandler);
-  //   server2.listen(PORT2, () => {
-  //     if (verbose) console.log(`Server running on http://localhost:${PORT2}`);
-  //   });
-  // } else {
-  //   if (verbose) console.log("Server is already running.");
-  // }
 }
 
 // Kill the server
